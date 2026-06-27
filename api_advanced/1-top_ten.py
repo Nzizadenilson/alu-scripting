c0efa06115e33d4f371a835d44d545e85f89aea5
Nzizadenilson <g.nziza@alustudent.com>
2026-06-27 21:47:09 +0200

    1-top_ten.py

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and prints the titles of the first 10 hot posts
+for a given subreddit.
+"""
+import requests
+
+def top_ten(subreddit):
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {
+        "User-Agent": "python:reddit.hotspots:v1.0"
+    }
+    try:
+        response = requests.get(
+            url,
+            headers=headers,
+            allow_redirects=False,
+            timeout=10
+        )
+        
+        if response.status_code != 200:
+            print(None)
+            return
+        data = response.json()
+        posts = data["data"]["children"]
+        for post in posts:
+            print(post["data"]["title"])
+    except (requests.RequestException, ValueError, KeyError):
+        print(None)
+
